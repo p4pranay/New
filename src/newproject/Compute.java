4120db6e781a80a55f12dca9e2ee12d56cc69667
sahas <sahas@LAPTOP-MGCORQ78.lan>
2017-01-16 16:11:12 -0600

    changed pay as pay1

--- src/newproject/Compute.java
+++ src/newproject/Compute.java
@@ -2,7 +2,7 @@ package newproject;
 
 public class Compute extends Demotest {
 
-	public void Pay(int amount) {
+	public void Pay1(int amount) {
 
 		System.out.println("the pay is " + amount);
 		// TODO Auto-generated method stub
